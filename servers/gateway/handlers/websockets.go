6cbf742c033239b0c756c0fabb35a8c99096d25a
Dave Stearns <dlsinfo@uw.edu>
2017-08-17 12:01:43 -0700

    starter file for notifying clients challenge

---
+++ servers/gateway/handlers/websockets.go
@@ -0,0 +1,17 @@
+package handlers
+
+//TODO: add a handler that upgrades clients to a WebSocket connection
+//and adds that to a list of WebSockets to notify when events are
+//read from the RabbitMQ server. Remember to synchronize changes
+//to this list, as handlers are called concurrently from multiple
+//goroutines.
+
+//TODO: start a goroutine that connects to the RabbitMQ server,
+//reads events off the queue, and broadcasts them to all of
+//the existing WebSocket connections. If you get an error
+//writing to the WebSocket, just close it and remove it
+//from the list (client went away without closing from
+//their end). Also make sure you start a read pump that
+//reads incoming control messages, as described in the
+//Gorilla WebSocket API documentation:
+//http://godoc.org/github.com/gorilla/websocket
