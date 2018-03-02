1cc3aa29ce743c35c06c193c46df8d22616709d9
Dave Stearns <dlsinfo@uw.edu>
2018-03-02 12:12:05 -0800

    add test for SummaryHandler testing response status code and content-type header

--- servers/gateway/handlers/summary_test.go
+++ servers/gateway/handlers/summary_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"strings"
 	"testing"
@@ -369,5 +371,23 @@ func TestFetchHTML(t *testing.T) {
 			stream.Close()
 		}
 	}
+}
 
+func TestSummaryHandler(t *testing.T) {
+	//verify that response has
+	// - correct response status code
+	// - correct Content-Type header
+	resp := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/summary?url=http://ogp.me", nil)
+	SummaryHandler(resp, req)
+	if resp.Code != http.StatusOK {
+		t.Errorf("incorrect response status code: expected %d but got %d", http.StatusOK, resp.Code)
+	}
+	expectedctype := "application/json"
+	ctype := resp.Header().Get("Content-Type")
+	if len(ctype) == 0 {
+		t.Errorf("No `Content-Type` header found in the response: must be there and must start with `%s`", expectedctype)
+	} else if !strings.HasPrefix(ctype, expectedctype) {
+		t.Errorf("incorrect `Content-Type` header value: expected it to start with `%s` but got `%s`", expectedctype, ctype)
+	}
 }
